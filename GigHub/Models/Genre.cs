a7f9d7cdf9e3b47f9d0f9a86694968f205f7d251
Mohit Vashishth <mohitkumar342@gmail.com>
2021-03-07 21:59:51 +0530

    Update Gig Model

---
+++ GigHub/Models/Genre.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GigHub.Models
+{
+    public class Genre
+    {
+        public int ID { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+    }
+}
