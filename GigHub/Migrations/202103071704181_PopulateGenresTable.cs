845d6383422bad5d8b1590a5b9fe5e37ef89d355
Mohit Vashishth <mohitkumar342@gmail.com>
2021-03-07 22:38:02 +0530

    Populate Genres Table

---
+++ GigHub/Migrations/202103071704181_PopulateGenresTable.cs
@@ -0,0 +1,21 @@
+namespace GigHub.Migrations
+{
+    using System.Data.Entity.Migrations;
+
+    public partial class PopulateGenresTable : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("INSERT INTO Genres ( Name) VALUES ('Jazz')");
+            Sql("INSERT INTO Genres ( Name) VALUES ('Blues')");
+            Sql("INSERT INTO Genres ( Name) VALUES ('Rock')");
+            Sql("INSERT INTO Genres ( Name) VALUES ('Country')");
+
+        }
+
+        public override void Down()
+        {
+            Sql("DELETE FROM Genres WHERE ID IN (1,2,3,4)");
+        }
+    }
+}
